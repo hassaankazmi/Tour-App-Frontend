10de02a28394f9815594e175f63e0542e92461f3
hassaankazmi <hassaankazzmi@gmail.com>
2025-07-10 12:59:12 +0500

    Initialize:  basic rotuting

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,12 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 
-
+const routes: Routes = [
+  {
+    path: '',
+    component:LoginComponent
+  },
+  {
+    path: '/signUp',
+    component:SignUpComponent
+  }
+]
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forRoot(routes),
+  ],
+  exports: [
+    RouterModule
   ]
 })
 export class AuthModule { }
--- src/app/tours/tours.module.ts
+++ src/app/tours/tours.module.ts
@@ -1,12 +1,35 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { Router } from 'express';
+import { ListingComponent } from './listing/listing.component';
+import { BookingComponent } from './booking/booking.component';
+import { ReviewsComponent } from './reviews/reviews.component';
 
 
+const routes: Routes = [
+  {
+    path: '',
+    component:ListingComponent
+  },
+  {
+    path: '/booking',
+    component:BookingComponent 
+  },
+  {
+    path: '/review',
+    component:ReviewsComponent
+  }
+]
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forRoot(routes),
+  ],
+  exports: [
+    RouterModule
   ]
 })
 export class ToursModule { }
