1fc0428e08eae6f4213025072c8ce3552bb40ffc
hassaankazmi <hassaankazzmi@gmail.com>
2025-07-10 12:58:34 +0500

    Initialize: basic routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadChildren: () => import('./auth/auth.module').then(module => module.AuthModule)
+    },
+    {
+        path: '/tour',
+        loadChildren: () => import('./tours/tours.module').then(module => module.ToursModule)
+    },
+
+    {
+        path: '**',
+        redirectTo:'/tour'
+    }
+    
+];
